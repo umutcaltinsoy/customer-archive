d63c8392fc754f6535110ef88fa8c1fa8a8807f3
umutaltinsoy <umutcaltinsoy@gmail.com>
2022-04-12 20:02:25 +0300

    Initial commit

---
+++ src/main/java/com/altinsoy/customerarchive/CustomerArchiveApplication.java
@@ -0,0 +1,13 @@
+package com.altinsoy.customerarchive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerArchiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerArchiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/altinsoy/customerarchive/CustomerArchiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.altinsoy.customerarchive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerArchiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
