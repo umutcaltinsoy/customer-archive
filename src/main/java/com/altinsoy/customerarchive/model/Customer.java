b3d48ab7652bcbb8897257963c2a7b4f0f2b8333
umutaltinsoy <umutcaltinsoy@gmail.com>
2022-04-14 00:11:36 +0300

    Customer

---
+++ src/main/java/com/altinsoy/customerarchive/model/Customer.java
@@ -0,0 +1,31 @@
+package com.altinsoy.customerarchive.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+@Getter
+@Setter
+@Entity
+@Table(name = "customers")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "identity_number")
+    private String identityNumber;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "email")
+    private String email;
+}
